b95f1acbd22c067c14fc853a3d7a6de2ce3ecc8a
Ashwin Sateesh Kumar <117780669+ashwin-sateesh@users.noreply.github.com>
2023-12-07 04:47:40 -0500

    Add files via upload

--- login.php
+++ login.php
@@ -9,23 +9,30 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Unable to connect to database"); // Hide specific error details from users
 }
 
 // Get form data
 $user = $_POST['username'];
 $pass = $_POST['password'];
 
-// SQL to check login
-$sql = "SELECT * FROM login_details WHERE username='$user' AND password='$pass'";
-$result = $conn->query($sql);
+// Prepare and bind
+$stmt = $conn->prepare("SELECT * FROM login_details WHERE username = ? AND password = ?");
+$stmt->bind_param("ss", $user, $pass); // 'ss' specifies the variable types are strings
+
+// Execute the statement
+$stmt->execute();
+$result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
     // Redirect to main.html if login is successful
     header('Location: main.html');
+    exit;
 } else {
     echo "Invalid username or password";
 }
 
+// Close statement and connection
+$stmt->close();
 $conn->close();
 ?>
